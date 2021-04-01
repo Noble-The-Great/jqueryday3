aa017da7a617e3d325c33fe7ede316bb931c1520
Noble Boucon <nobleboucon@gmail.com>
2021-03-31 20:31:36 -0600

    main try

---
+++ box.js
@@ -0,0 +1,22 @@
+
+
+
+
+for(i=0;i<16;i++){
+	$('.boxgroup').append(`<div class='box'> </div>`);
+};
+
+
+
+
+$('.box').on('click',function(){
+
+
+
+ 
+ $(this).toggleClass('blackie');
+
+
+
+});
+
